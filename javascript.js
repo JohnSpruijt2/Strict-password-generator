7009b4fbc9d45ce735988b81eedb54300ca78bbe
John Spruijt <99058314@mydavinci.nl>
2019-12-11 14:14:14 +0100

    added coloured icons & username input

--- javascript.js
+++ javascript.js
@@ -4,5 +4,12 @@ function handler() {
 }
 function handler2() {
     var boba = document.querySelector('input[id="password"]').value
-   console.log(boba)
+   if (boba.length >0 && boba.length <9) {
+       console.log("groter dan 0")
+       document.getElementById("error").innerHTML = "<span class='negative'><i class='fas fa-times'></i></span> Password needs to be at least 8 char"
+   }
+   else if (boba.length >7) {
+        console.log("gelijk of groter dan 8")
+        document.getElementById("error").innerHTML = "<span class='positive'><i class='fas fa-check'></i></i></span> Password needs to be at least 8 char"
+   }
 }
