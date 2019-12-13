c7621b3469c497d1e935e458c5908d19b71bfeac
John Spruijt <99058314@mydavinci.nl>
2019-12-13 09:01:23 +0100

    added button and more errors

--- javascript.js
+++ javascript.js
@@ -20,13 +20,14 @@ function handler2() {
    else if (secret.length == 0) {
         document.getElementById("error1").innerHTML = ""
         document.getElementById("error2").innerHTML = ""
+        document.getElementById("error3").innerHTML = ""
    }
 }
 function handler3() {
     if (check1 == true) {
         document.getElementById("error2").innerHTML = "<span class='negative'><i class='fas fa-times'></i></span> Password needs to contain at least 2 uppercase letters"
     window.uppercase = 0
-    var split = secret.split("")
+    window.split = secret.split("")
     for (i=0; i<split.length; i++) {
         if (isNaN(split[i]) == true) {
             if (split[i] == split[i].toUpperCase()) {
@@ -40,8 +41,31 @@ function handler3() {
 function handler4() {
     if (uppercase >1) {
         document.getElementById("error2").innerHTML = "<span class='positive'><i class='fas fa-check'></i></i></span> Password needs to contain at least 2 uppercase letters"
+        handler5()
     }
     else {
-        "<span class='negative'><i class='fas fa-times'></i></span> Password needs to contain at least 2 uppercase letters"
+        document.getElementById("error2").innerHTML = "<span class='negative'><i class='fas fa-times'></i></span> Password needs to contain at least 2 uppercase letters"
     }
+}
+function handler5() {
+    var number = 0
+    for (i=0; i<split.length; i++) {
+        if (split[i] == 0 || split[i] == 1 || split[i] == 2 || split[i] == 3 || split[i] == 4 || split[i] == 5 || split[i] == 6 || split[i] == 7 || split[i] == 8 || split[i] == 9) {
+            number++
+        }
+        console.log(number)
+    }
+    if (number >= 3) {
+        document.getElementById("error3").innerHTML = "<span class='positive'><i class='fas fa-check'></i></i></span> Password needs to contain at least 3 numbers"
+        handler6()
+    }
+    else {
+        document.getElementById("error3").innerHTML = "<span class='negative'><i class='fas fa-times'></i></span> Password needs to contain at least 3 numbers"
+    }
+}
+function handler6() {
+    
+}
+function submit() {
+
 }
